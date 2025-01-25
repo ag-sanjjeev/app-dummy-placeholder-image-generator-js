c12cefc96b4331167acd559ea89406980179acca
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2025-01-25 16:07:53 +0530

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/*
+ *  Dummy Image Generator
+ *  
+ *  Copyright 2025 ag-sanjjeev               		
+ * 
+ *  The source code is licensed under  MIT-style 
+ *  License. The usage, permission and condition 
+ *  are applicable  to  this source code  as per 		
+ *  license.  That can be found in  LICENSE file
+ *  or at https://opensource.org/licenses/MIT.			
+ * 
+ */
+
+ 
